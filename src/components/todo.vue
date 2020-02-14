1d39594433728aeda3b459edab4585c55a3ef25a
승가리 <imsa1224@korea.ac.kr>
2020-02-14 23:20:45 +0900

    1st commit to existed local pj

---
+++ src/components/todo.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="list-group-item text-left" @click="clickButton">{{ label }}</button>
+</template>
+<script>
+export default {
+  props: ["label"],
+  methods: {
+    clickButton() {
+      this.$emit("componentClick");
+    }
+  }
+};
+</script>
